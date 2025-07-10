af7cc1d37f381999e4e4258bab3193f895e5c536
Dmitry Kovalevsy <dkovalevsy@dbi.ru>
2025-07-10 18:31:15 +0300

    создал проект и добавил стартовые элементы

---
+++ ConfiguringApps/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Dictionary<string, string>()
+            {
+                ["Message"] = "This is the Index action"
+            });
+        }
+    }
+}
